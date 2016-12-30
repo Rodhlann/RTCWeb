dde12a9aac513a9239522b0872045232ff0c4799
James Flesher <james.d.flesher@gmail.com>
2016-12-29 23:42:59 -0500

    added query to collect all Categories; fixed java version target issue to enable lambda and other features; added paging to acquire all results return from work item query

---
+++ rtc-web-api/src/main/java/com/solo/tech/rtc/web/WorkItemStatus.java
@@ -0,0 +1,37 @@
+package com.solo.tech.rtc.web;
+
+import org.springframework.util.StringUtils;
+
+/**
+ * Created by jamesflesher on 12/29/16.
+ */
+public enum WorkItemStatus {
+    READY_FOR_DEVELOPMENT("Ready For Development"),
+    IN_PROGRESS("In Progress"),
+    IN_SCRUM_TEST("In Scrum Test"),
+    DONE("Done"),
+    UNKNOWN("Unknown");
+
+    private String statusText;
+
+    WorkItemStatus(String value) {
+        statusText = value;
+    }
+
+    public String getStatusText() { return statusText; }
+
+    public static WorkItemStatus fromString(String value) {
+        WorkItemStatus status = UNKNOWN;
+
+        if(!StringUtils.isEmpty(value)) {
+            for(WorkItemStatus type : WorkItemStatus.values()) {
+                if(type.statusText.equalsIgnoreCase(value)) {
+                    status = type;
+                    break;
+                }
+            }
+        }
+
+        return status;
+    }
+}
